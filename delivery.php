ea52c9428c4d0555ffac962859ad90c04670f1b7
Chaerin Hwang <chaa00@naver.com>
2023-11-07 14:55:36 +0900

    배송/상품 페이지 구현

---
+++ delivery.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html><!--  Last Published: Sun Nov 05 2023 10:17:38 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="6545eca4ff3677790e075d2f" data-wf-site="6541d9f4fbd73e9498319e4c">
+
+<head>
+  <meta charset="utf-8">
+  <title>ThinkBig : Shipping Management</title>
+  <meta content="Delivery" property="og:title">
+  <meta content="Delivery" property="twitter:title">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/hahas-supercool-site.webflow.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    WebFont.load({
+      google: {
+        families: ["Changa One:400,400italic"]
+      }
+    });
+  </script>
+  <script type="text/javascript">
+    ! function (o, c) {
+      var n = c.documentElement,
+        t = " w-mod-";
+      n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
+    }(window, document);
+  </script>
+  <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+</head>
+
+<body>
+  <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease"
+    role="banner" class="navbar w-nav">
+    <div class="navbar-wrapper w-container">
+      <a href="newnotice.html" class="nav-btn w-nav-link">NOTICE</a>
+      <a href="user.html" class="nav-btn w-nav-link">USER</a>
+      <a href="brand.html" class="nav-btn w-nav-link">BRAND</a>
+      <a href="product.php" class="nav-btn w-nav-link">PRODUCT</a>
+      <a href="delivery.php" aria-current="page" class="nav-btn w-nav-link w--current">DELIVERY</a>
+      <a href="review.html" class="nav-btn w-nav-link">REVIEW</a>
+      <a href="coupontemp2.html" class="nav-btn w-nav-link">COUPON</a>
+      <a href="qna.html" class="nav-btn w-nav-link">Q&amp;A</a>
+    </div>
+  </div>
+  <section class="titlebar">
+    <a href="index.html" class="link-block w-inline-block"><img src="images/ThinkBig.png" loading="lazy" alt=""
+        class="image"></a>
+  </section>
+  <div id="w-node-_01990053-9cad-eed2-2fd0-c51042324ab2-0e075d2f" class="w-layout-layout block wf-layout-layout">
+    <div id="w-node-_01990053-9cad-eed2-2fd0-c51042324ab3-0e075d2f" class="w-layout-cell userinfo"><img
+        src="images/ThinkBig.png" loading="lazy" width="100" height="100" alt="" class="userimage">
+      <div id="w-node-_01990053-9cad-eed2-2fd0-c51042324ab5-0e075d2f" class="w-layout-layout info wf-layout-layout">
+        <div id="w-node-_01990053-9cad-eed2-2fd0-c51042324ab6-0e075d2f" class="w-layout-cell id">
+          <div class="user-info">Salary123</div>
+        </div>
+        <div id="w-node-_01990053-9cad-eed2-2fd0-c51042324ab9-0e075d2f" class="w-layout-cell name">
+          <div class="user-info">matilda</div>
+        </div>
+        <div id="w-node-_01990053-9cad-eed2-2fd0-c51042324abc-0e075d2f" class="w-layout-cell department">
+          <div class="user-info">management</div>
+        </div>
+      </div>
+    </div>
+    <div id="deliverycell"
+      class="w-layout-cell content deliverycell w-node-_01990053-9cad-eed2-2fd0-c51042324abf-0e075d2f">
+      <div class="div-block-19 deliverynav">
+        <h4 id="delivery_userinfo_col" class="heading-2">INFO</h4>
+        <h4 id="delivery_status_col" class="heading-3">STATUS</h4>
+        <h4 id="delivery_changestatus_col" class="heading-4">CHANGE STATUS</h4>
+      </div>
+
+      <?php
+      $mysqli = mysqli_connect("localhost:3307", "team10", "team10", "team10");
+
+      if (mysqli_connect_errno()) {
+        echo "" . mysqli_connect_error();
+        exit();
+      } else {
+        $sql = "select * from deliveryView";
+        $res = mysqli_query($mysqli, $sql);
+        if ($res) {
+          while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+            $orderID = $row["orderID"];
+            $userID = $row["userID"];
+            $orderDate = $row["orderDate"];
+            $addr = $row["address"];
+            $brandName = $row["brandName"];
+            $productName = $row["productName"];
+            $orderComment = $row["comment"];
+            $deliveryStatus = $row["status"];
+
+            echo "<div class='div-block-14'>
+                <div class='div-block-16 deliveryinfo'>
+                  <ul role='list' class='list'>
+                    <li class='list-item-2' style='list-style:none;'>USER ID : " . $userID . "</li>
+                    <li class='list-item-3' style='list-style:none;'>ORDER DATE : " . $orderDate . "</li>
+                    <li class='list-item-4' style='list-style:none;'>ADDRESS : " . $addr . "</li>
+                    <li class='list-item-6' style='list-style:none;'>BRAND NAME : " . $brandName . "</li>
+                    <li class='list-item-7' style='list-style:none;'>PRODUCT NAME : " . $productName . "</li>
+                    <li class='list-item-8' style='list-style:none;'>ORDER COMMENT : " . $orderComment . "</li>
+                  </ul>
+                </div>
+              <div class='div-block-17'>
+                <div class='text-block-2'>" . $deliveryStatus . "</div>
+              </div>";
+
+            if ($deliveryStatus == 'paid') {
+              echo "<div class='div-block-18 deliverydata1_changeblock'>
+              <form action='deliveryUpdate.php' method='POST'>
+                <input type='hidden' name='orderID' value='" . $orderID . "'>
+                <button name='deliveryStatus' type='submit' value='paid' class='paid selectdelivery w-button'>PAID</button>
+                <button name='deliveryStatus' type='submit' value='ready' class='ready w-button'>READY</button>
+                <button name='deliveryStatus' type='submit' value='shipping' class='shipping w-button'>SHIPPING</button>
+                <button name='deliveryStatus' type='submit' value='done' class='done w-button'>DONE</button>
+              </form>
+              </div>
+              </div>";
+            }
+            if ($deliveryStatus == 'ready') {
+              echo "<div class='div-block-18 deliverydata1_changeblock'>
+              <form action='deliveryUpdate.php' method='POST'>
+                <input type='hidden' name='orderID' value='" . $orderID . "'>
+                <button name='deliveryStatus' type='submit' value='paid' class='paid w-button'>PAID</button>
+                <button name='deliveryStatus' type='submit' value='ready' class='ready selectdelivery w-button'>READY</button>
+                <button name='deliveryStatus' type='submit' value='shipping' class='shipping w-button'>SHIPPING</button>
+                <button name='deliveryStatus' type='submit' value='done' class='done w-button'>DONE</button>
+              </form>
+              </div>
+              </div>";
+            }
+            if ($deliveryStatus == 'shipping') {
+              echo "<div class='div-block-18 deliverydata1_changeblock'>
+              <form action='deliveryUpdate.php' method='POST'>
+                <input type='hidden' name='orderID' value='".$orderID."'>
+                <button name='deliveryStatus' type='submit' value='paid' class='paid w-button'>PAID</button>
+                <button name='deliveryStatus' type='submit' value='ready' class='ready w-button'>READY</button>
+                <button name='deliveryStatus' type='submit' value='shipping' class='shipping selectdelivery w-button'>SHIPPING</button>
+                <button name='deliveryStatus' type='submit' value='done' class='done w-button'>DONE</button>
+              </form>
+              </div>
+              </div>";
+            }
+            if ($deliveryStatus == 'done') {
+              echo "<div class='div-block-18 deliverydata1_changeblock'>
+              <form action='deliveryUpdate.php' method='POST'>
+                <input type='hidden' name='orderID' value='".$orderID."'>
+                <button name='deliveryStatus' type='submit' value='paid' class='paid w-button'>PAID</button>
+                <button name='deliveryStatus' type='submit' value='ready' class='ready w-button'>READY</button>
+                <button name='deliveryStatus' type='submit' value='shipping' class='shipping w-button'>SHIPPING</button>
+                <button name='deliveryStatus' type='submit' value='done' class='done selectdelivery w-button'>DONE</button>
+              </form>
+              </div>
+              </div>";
+            }
+
+          }
+        } else {
+          printf("%s", mysqli_error($mysqli));
+        }
+        mysqli_free_result($res);
+        mysqli_close($mysqli);
+      }
+      ?>
+    </div>
+    <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=6541d9f4fbd73e9498319e4c"
+      type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+      crossorigin="anonymous"></script>
+    <script src="js/webflow.js" type="text/javascript"></script>
+</body>
+
+</html>
---
+++ productSorting_excluding_SoldOut.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html><!--  Last Published: Mon Nov 06 2023 05:41:27 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="6545eca4ff3677790e075d32" data-wf-site="6541d9f4fbd73e9498319e4c">
+
+<head>
+    <meta charset="utf-8">
+    <title>Product</title>
+    <meta content="Product" property="og:title">
+    <meta content="Product" property="twitter:title">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="css/normalize.css" rel="stylesheet" type="text/css">
+    <link href="css/webflow.css" rel="stylesheet" type="text/css">
+    <link href="css/hahas-supercool-site.webflow.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        WebFont.load({
+            google: {
+                families: ["Changa One:400,400italic"]
+            }
+        });
+    </script>
+    <script type="text/javascript">
+        ! function (o, c) {
+            var n = c.documentElement,
+                t = " w-mod-";
+            n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
+        }(window, document);
+    </script>
+    <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon">
+    <link href="images/webclip.png" rel="apple-touch-icon">
+</head>
+
+<body>
+    <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease"
+        role="banner" class="navbar w-nav">
+        <div class="navbar-wrapper w-container">
+            <a href="notice.html" class="nav-btn w-nav-link">NOTICE</a>
+            <a href="user.html" class="nav-btn w-nav-link">USER</a>
+            <a href="brand.html" class="nav-btn w-nav-link">BRAND</a>
+            <a href="product.php" aria-current="page" class="nav-btn w-nav-link w--current">PRODUCT</a>
+            <a href="delivery.php" class="nav-btn w-nav-link">DELIVERY</a>
+            <a href="review.html" class="nav-btn w-nav-link">REVIEW</a>
+            <a href="coupon.html" class="nav-btn w-nav-link">COUPON</a>
+            <a href="qna.html" class="nav-btn w-nav-link">Q&amp;A</a>
+        </div>
+    </div>
+    <section class="titlebar">
+        <a href="index.html" class="link-block w-inline-block"><img src="images/ThinkBig.png" loading="lazy" alt=""
+                class="image"></a>
+    </section>
+    <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11bc-0e075d32" class="w-layout-layout block wf-layout-layout">
+        <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11bd-0e075d32" class="w-layout-cell userinfo"><img
+                src="images/ThinkBig.png" loading="lazy" width="100" height="100" alt="" class="userimage">
+            <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11bf-0e075d32"
+                class="w-layout-layout info wf-layout-layout">
+                <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11c0-0e075d32" class="w-layout-cell id">
+                    <div class="user-info">Salary123</div>
+                </div>
+                <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11c3-0e075d32" class="w-layout-cell name">
+                    <div class="user-info">matilda</div>
+                </div>
+                <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11c6-0e075d32" class="w-layout-cell department">
+                    <div class="user-info">management</div>
+                </div>
+            </div>
+        </div>
+        <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11c9-0e075d32" class="w-layout-cell content productcell">
+            <div class="div-block-5">
+                <div class="w-form" style="display:flex;">
+                    <form id="productform" action="productSorting_excluding_SoldOut.php" method="POST"
+                        style="width:1070px;">
+                        <select onchange="this.form.submit()" id="productfiled" name="sortBy" data-name="Field"
+                            class="select-field w-select">
+                            <?php
+                            $sortBy = $_POST['sortBy'];
+                            if ($sortBy == 'wishes') {
+                                echo '<option value="wishes" selected>Wishes</option>
+                                <option value="highestPrice">Highest Price</option>
+                                <option value="lowestPrice">Lowest Price</option>
+                                <option value="order">Order Quantity</option>
+                                <option value="rating">Rating</option>';
+                            }
+                            if ($sortBy == 'highestPrice') {
+                                echo '<option value="wishes" >Wishes</option>
+                                <option value="highestPrice" selected>Highest Price</option>
+                                <option value="lowestPrice">Lowest Price</option>
+                                <option value="order">Order Quantity</option>
+                                <option value="rating">Rating</option>';
+                            }
+                            if ($sortBy == 'lowestPrice') {
+                                echo '<option value="wishes" >Wishes</option>
+                                <option value="highestPrice">Highest Price</option>
+                                <option value="lowestPrice" selected>Lowest Price</option>
+                                <option value="order">Order Quantity</option>
+                                <option value="rating">Rating</option>';
+                            }
+                            if ($sortBy == 'order') {
+                                echo '<option value="wishes" >Wishes</option>
+                                <option value="highestPrice">Highest Price</option>
+                                <option value="lowestPrice">Lowest Price</option>
+                                <option value="order" selected>Order Quantity</option>
+                                <option value="rating">Rating</option>';
+                            }
+                            if ($sortBy == 'rating') {
+                                echo '<option value="wishes" >Wishes</option>
+                                <option value="highestPrice">Highest Price</option>
+                                <option value="lowestPrice">Lowest Price</option>
+                                <option value="order">Order Quantity</option>
+                                <option value="rating" selected>Rating</option>';
+                            }
+                            ?>
+                        </select>
+                    </form>
+                    <form action="productSorting.php" method="POST" style="display:flex;">
+                        <?php
+                            echo '<input type="hidden" name="sortBy" value="' . $sortBy . '">';
+                        ?>
+                        <input onchange="this.form.submit()" type="checkbox" name="soldOut" value="Y"
+                            style="margin-left:10px; margin-right:5px;" checked><label for="soldOut"
+                            style="padding-top:13px;">Excluding sold-out</label>
+                    </form>
+                    <div class="w-form-done">
+                        <div>Thank you! Your submission has been received!</div>
+                    </div>
+                    <div class="w-form-fail">
+                        <div>Oops! Something went wrong while submitting the form.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="div-block-6">
+                <div id="productcols" class="div-block-8 div-product">
+                    <div id="product_rank_col" class="div-block-7">RANK</div>
+                    <div id="product_productname_col" class="div-block-7 productnamecol">PRODUCT NAME </div>
+                    <div id="product_brandname_col" class="div-block-7 brandnamecol">BRAND NAME</div>
+                    <div id="product_price_col" class="div-block-7">TARGET</div>
+                    <?php
+                    if ($sortBy == "wishes") {
+                        echo '<div id="product_price_col" class="div-block-7">wishCount</div>';
+                    }
+                    if ($sortBy == "highestPrice" || $sortBy == "lowestPrice") {
+                        echo '<div id="product_price_col" class="div-block-7">price</div>';
+                    }
+                    if ($sortBy == "order") {
+                        echo '<div id="product_price_col" class="div-block-7">orderCount</div>';
+                    }
+                    if ($sortBy == "rating") {
+                        echo '<div id="product_price_col" class="div-block-7">avgRating</div>';
+                    }
+                    ?>
+
+                </div>
+                <div class="productlists">
+                    <?php
+                    $mysqli = mysqli_connect("localhost:3307", "team10", "team10", "team10");
+
+                    if (mysqli_connect_errno()) {
+                        echo "" . mysqli_connect_error();
+                        exit();
+                    } else {
+                        if ($sortBy == "wishes") {
+                            $sql = "select RANK() over (ORDER BY  count desc) as 'rank',  productName,brandName,target,count as fivParam from wishCount natural join products natural join brands where stock!=0";
+                        }
+                        if ($sortBy == 'highestPrice') {
+                            $sql = "select RANK() over (ORDER BY price desc) as 'rank',productName, brandName, target, price as fivParam from products natural join brands where stock!=0";
+                        }
+                        if ($sortBy == "lowestPrice") {
+                            $sql = "select RANK() over (ORDER BY price asc) as 'rank',productName, brandName, target, price as fivParam from products natural join brands where stock!=0";
+                        }
+                        if ($sortBy == "order") {
+                            $sql = "select RANK() over (ORDER BY  count desc) as 'rank',  productName,brandName,target,count as fivParam from orderCount natural join products natural join brands where stock!=0";
+                        }
+                        if ($sortBy == "rating") {
+                            $sql = "select RANK() over (ORDER BY  rating desc) as 'rank',  productName,brandName,target,rating as fivParam from reviewRating natural join products natural join brands where stock!=0";
+                        }
+                        $res = mysqli_query($mysqli, $sql);
+                        if ($res) {
+                            while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+                                $rank = $row["rank"];
+                                $productName = $row['productName'];
+                                $brandName = $row['brandName'];
+                                $target = $row['target'];
+                                $param = $row['fivParam'];
+                                echo "
+                                    <div class='div-block-8'>
+                                        <div class='div-block-7 datarow'>" . $rank . "</div>
+                                        <div class='div-block-7 productnamecol datarow'>" . $productName . "</div>
+                                        <div class='div-block-7 brandnamecol datarow'>" . $brandName . "</div>
+                                        <div class='div-block-7 datarow'>" . $target . "</div>
+                                        <div class='div-block-7 datarow'>" . $param . "</div>
+                                    </div>
+                                ";
+                            }
+                        } else {
+                            printf("%s", mysqli_error($mysqli));
+                        }
+                        mysqli_free_result($res);
+                        mysqli_close($mysqli);
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=6541d9f4fbd73e9498319e4c"
+        type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
+    <script src="js/webflow.js" type="text/javascript"></script>
+</body>
+
+</html>
