22681e0e66c18c839b4f3791546d1b5f9b72972d
bboyeong <100745018+bboyeong@users.noreply.github.com>
2023-11-11 17:40:53 +0900

    Update by boyeong

--- product.php
+++ product.php
@@ -35,14 +35,14 @@
   <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease"
     role="banner" class="navbar w-nav">
     <div class="navbar-wrapper w-container">
-      <a href="notice.html" class="nav-btn w-nav-link">NOTICE</a>
-      <a href="user.html" class="nav-btn w-nav-link">USER</a>
-      <a href="brand.html" class="nav-btn w-nav-link">BRAND</a>
+      <a href="notice.php" class="nav-btn w-nav-link">NOTICE</a>
+      <a href="user.php" class="nav-btn w-nav-link">USER</a>
+      <a href="brand.php" class="nav-btn w-nav-link">BRAND</a>
       <a href="product.php" aria-current="page" class="nav-btn w-nav-link w--current">PRODUCT</a>
       <a href="delivery.php" class="nav-btn w-nav-link">DELIVERY</a>
-      <a href="review.html" class="nav-btn w-nav-link">REVIEW</a>
-      <a href="coupon.html" class="nav-btn w-nav-link">COUPON</a>
-      <a href="qna.html" class="nav-btn w-nav-link">Q&amp;A</a>
+      <a href="review.php" class="nav-btn w-nav-link">REVIEW</a>
+      <a href="coupon.php" class="nav-btn w-nav-link">COUPON</a>
+      <a href="qna.php" class="nav-btn w-nav-link">Q&amp;A</a>
     </div>
   </div>
   <section class="titlebar">
@@ -54,13 +54,13 @@
         src="images/ThinkBig.png" loading="lazy" width="100" height="100" alt="" class="userimage">
       <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11bf-0e075d32" class="w-layout-layout info wf-layout-layout">
         <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11c0-0e075d32" class="w-layout-cell id">
-          <div class="user-info">Salary123</div>
+          <div class="user-info"><?php session_start(); echo $_SESSION['employeeID'];?></div>
         </div>
         <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11c3-0e075d32" class="w-layout-cell name">
-          <div class="user-info">matilda</div>
+          <div class="user-info"><?php  echo $_SESSION['name'];?></div>
         </div>
         <div id="w-node-_17ab5aa1-19f5-d367-e3c3-21d7eecd11c6-0e075d32" class="w-layout-cell department">
-          <div class="user-info">management</div>
+          <div class="user-info"><?php echo $_SESSION['department'];?></div>
         </div>
       </div>
     </div>
